6dbdd2780e2fb60af7f77a505e7f5ad50fb01b51
0xAndrewBlack <0xandrewblack@gmail.com>
2022-04-09 02:32:26 +0200

    📂 Adatbázis + README.

--- db/musicpedia.sql
+++ db/musicpedia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 08, 2022 at 05:05 PM
+-- Generation Time: Apr 09, 2022 at 02:24 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `musicpedia`
 --
-CREATE DATABASE IF NOT EXISTS `musicpedia` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
-USE `musicpedia`;
 
 -- --------------------------------------------------------
 
@@ -34,15 +32,15 @@ CREATE TABLE `messages` (
   `time` datetime NOT NULL COMMENT 'sent at',
   `sender` int(11) NOT NULL COMMENT 'sender',
   `recipient` int(11) NOT NULL COMMENT 'recipient',
-  `message_content` text COLLATE utf8_hungarian_ci NOT NULL COMMENT 'message'
+  `message` text COLLATE utf8_hungarian_ci NOT NULL COMMENT 'message'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
 -- Dumping data for table `messages`
 --
 
-INSERT INTO `messages` (`id`, `time`, `sender`, `recipient`, `message_content`) VALUES
-(1, '2022-04-08 17:02:10', 1, 2, 'Hali, Teszt2!');
+INSERT INTO `messages` (`id`, `time`, `sender`, `recipient`, `message`) VALUES
+(1, '2022-04-08 17:02:10', 1, 2, 'Hali, Teszt!');
 
 -- --------------------------------------------------------
 
@@ -55,20 +53,22 @@ CREATE TABLE `users` (
   `registration_date` datetime NOT NULL COMMENT 'registration date',
   `email` varchar(64) COLLATE utf8_hungarian_ci NOT NULL COMMENT 'email address',
   `password` varchar(256) COLLATE utf8_hungarian_ci NOT NULL COMMENT 'hashed password',
-  `about_me` text COLLATE utf8_hungarian_ci NOT NULL COMMENT 'user about message',
-  `keresztnev` text COLLATE utf8_hungarian_ci NOT NULL COMMENT 'keresztnév',
-  `vezeteknev` text COLLATE utf8_hungarian_ci NOT NULL COMMENT 'vezetéknév',
-  `brithdate` date DEFAULT NULL COMMENT 'születési dátum',
-  `priviliege_level` int(11) NOT NULL COMMENT 'jogi szint 0 user 1 eloado 10 admin'
+  `about` text COLLATE utf8_hungarian_ci NOT NULL DEFAULT 'Hello Musicpedia!' COMMENT 'user about message',
+  `pfp` text COLLATE utf8_hungarian_ci NOT NULL DEFAULT 'default.png' COMMENT 'profile picture filename',
+  `firstname` text COLLATE utf8_hungarian_ci NOT NULL COMMENT 'keresztnév',
+  `lastname` text COLLATE utf8_hungarian_ci NOT NULL COMMENT 'vezetéknév',
+  `birthdate` date DEFAULT NULL COMMENT 'születési dátum',
+  `priviliege_level` int(11) NOT NULL DEFAULT 0 COMMENT 'jogosultsagi szint 0 user, 1 eloado, 10 admin'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `registration_date`, `email`, `password`, `about_me`, `keresztnev`, `vezeteknev`, `brithdate`, `priviliege_level`) VALUES
-(1, '2022-04-08 16:12:36', 'test@domain.tld', '12345', 'Hello', 'John', 'Doe', '2022-04-01', 0),
-(2, '2022-04-08 17:02:50', 'test2@domain.tld', '12345', 'Hello2', 'Jane', 'Doe', '2022-04-01', 0);
+INSERT INTO `users` (`id`, `registration_date`, `email`, `password`, `about`, `pfp`, `firstname`, `lastname`, `birthdate`, `priviliege_level`) VALUES
+(0, '2022-04-09 01:58:00', 'admin@musicpedia.hu', '$2y$10$.AI82ZlCBaKVDiXwj/AGHecs9CD1XLaMLGHacUDpmhGFZ3f/WGZuC', 'Hello Musicpedia!', 'default.png', 'Admin', 'Fiók', '2070-04-01', 10),
+(2, '2022-04-09 01:56:53', 'test@example.com', '$2y$10$lTK3MGAbbcl1phwNz7CQVeb5CDt6IC83NF8i89xan5DegChwUnsNq', 'Hello Musicpedia!', 'default.png', 'Teszt', 'Elek', '2002-04-01', 0),
+(1, '2022-04-09 01:57:35', 'user@musicpedia.hu', '$2y$10$Vc2ymDzvEnydA7BblRRtc.Dvjk7El59qPsuXp5MWTfThTbyFtd1ka', 'Hello Musicpedia!', 'default.png', 'Alap', 'Felhasználó', '2001-04-01', 0);
 
 --
 -- Indexes for dumped tables
@@ -78,13 +78,15 @@ INSERT INTO `users` (`id`, `registration_date`, `email`, `password`, `about_me`,
 -- Indexes for table `messages`
 --
 ALTER TABLE `messages`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`email`),
+  ADD KEY `id` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ docs/README.md
@@ -0,0 +1,32 @@
+# MUSICPEDIA
+
+> Webterv gyak. projektmunka
+
+---
+
+## Beüzemelés
+
+### Adatbázis
+
+A `db` mappában található `musicpedia.sql` fájlt importáld egy tetszőleges MySQL szerverbe (XAMPP ajánlott).
+Alap importálási beállításokkal működni fog.
+
+### Alap felhasználók
+
+> Ezek a fiókok demó céljából készültek, semmi kapcsolat nincs a valóélet és a projekt között, ha bármi is lenne az pusztán a véletlen műve. 👀
+
+| E-Mail              | Jelszó     |
+| ------------------- | ---------- |
+| test@example.com    | t3sTus3r   |
+| user@musicpedia.hu  | d3Faus3r   |
+| admin@musicpedia.hu | mus1cP3di4 |
+
+---
+
+## Készítették
+
+Fekete András
+
+Kalóczkai Nikoletta
+
+---
