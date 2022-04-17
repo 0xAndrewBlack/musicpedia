45971a72138d067727a5bbe4e8f81829fedb8820
0xAndrewBlack <0xandrewblack@gmail.com>
2022-04-17 15:45:29 +0200

    🍬 Frissítések.

--- db/musicpedia.sql
+++ db/musicpedia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 09, 2022 at 01:45 PM
+-- Generation Time: Apr 17, 2022 at 03:40 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -28,8 +28,6 @@ USE `musicpedia`;
 --
 -- Table structure for table `messages`
 --
--- Creation: Apr 09, 2022 at 11:28 AM
---
 
 CREATE TABLE `messages` (
   `id` int(11) NOT NULL COMMENT 'message id',
@@ -40,23 +38,20 @@ CREATE TABLE `messages` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
--- RELATIONSHIPS FOR TABLE `messages`:
---
-
---
 -- Dumping data for table `messages`
 --
 
 INSERT INTO `messages` (`id`, `time`, `sender`, `recipient`, `message`) VALUES
-(1, '2022-04-08 17:02:10', 1, 2, 'Hali, Teszt!');
+(1, '2022-04-08 17:02:10', 1, 2, 'Hali, Teszt!'),
+(2, '2022-04-17 00:32:06', 1, 0, 'Hali Admin!'),
+(3, '2022-04-17 00:50:05', 0, 0, 'Hali'),
+(4, '2022-04-17 00:50:26', 0, 2, 'Hali 2'),
+(5, '2022-04-17 15:40:11', 0, 0, 'Hali saját magam :D');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
---
--- Creation: Apr 09, 2022 at 11:43 AM
--- Last update: Apr 09, 2022 at 11:43 AM
 --
 
 CREATE TABLE `users` (
@@ -68,22 +63,20 @@ CREATE TABLE `users` (
   `pfp` text COLLATE utf8_hungarian_ci NOT NULL DEFAULT 'default.png' COMMENT 'profile picture filename',
   `firstname` text COLLATE utf8_hungarian_ci NOT NULL COMMENT 'keresztnév',
   `lastname` text COLLATE utf8_hungarian_ci NOT NULL COMMENT 'vezetéknév',
-  `birthdate` date DEFAULT NULL COMMENT 'születési dátum',
-  `privilege_level` int(11) NOT NULL DEFAULT 0 COMMENT 'jogosultsagi szint 0 user, 1 eloado, 10 admin'
+  `birthdate` date NOT NULL DEFAULT current_timestamp() COMMENT 'születési dátum',
+  `privilege_level` int(11) NOT NULL DEFAULT 0 COMMENT 'jogosultsagi szint 0 user, 1 eloado, 10 admin',
+  `favorites` varchar(255) COLLATE utf8_hungarian_ci DEFAULT NULL COMMENT 'kedvenc előadók',
+  `privacy` int(11) DEFAULT NULL COMMENT 'privacy'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
---
--- RELATIONSHIPS FOR TABLE `users`:
---
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `registration_date`, `email`, `password`, `about`, `pfp`, `firstname`, `lastname`, `birthdate`, `privilege_level`) VALUES
-(0, '2022-04-09 13:40:35', 'admin@musicpedia.hu', '$2y$10$YVdhcB51lskhybSvIEx.NuxT/kn.oqUai2gh0sGXy3oBys4aQtUHu', 'Hello Musicpedia!', 'default.png', 'Admin', 'Fiók', '2077-04-01', 10),
-(1, '2022-04-09 13:41:39', 'user@musicpedia.hu', '$2y$10$cv0q1/qJawjxIg7OFPo/7ugH3Unln2snnyscXgFSiBEX4sGKh0yuq', 'Hello Musicpedia!', 'default.png', 'Alap', 'Felhasználó', '2001-04-01', 0),
-(2, '2022-04-09 13:42:15', 'test@example.com', '$2y$10$p44lGEPEGXVQ0oQefUrjNu.eEwLqwUj/xc6FjiKFsgbCJg636wjN2', 'Hello Musicpedia!', 'default.png', 'Teszt', 'Elek', '2002-04-01', 0);
+INSERT INTO `users` (`id`, `registration_date`, `email`, `password`, `about`, `pfp`, `firstname`, `lastname`, `birthdate`, `privilege_level`, `favorites`, `privacy`) VALUES
+(0, '2022-04-09 13:40:35', 'admin@musicpedia.hu', '$2y$10$YVdhcB51lskhybSvIEx.NuxT/kn.oqUai2gh0sGXy3oBys4aQtUHu', 'Hello Musicpedia!', 'default.png', 'Admin', 'Fiók', '2077-04-01', 10, NULL, NULL),
+(1, '2022-04-09 13:41:39', 'user@musicpedia.hu', '$2y$10$cv0q1/qJawjxIg7OFPo/7ugH3Unln2snnyscXgFSiBEX4sGKh0yuq', 'Hello Musicpedia!', 'default.png', 'Alap', 'Felhasználó', '2001-04-01', 0, NULL, NULL),
+(2, '2022-04-09 13:42:15', 'test@example.com', '$2y$10$p44lGEPEGXVQ0oQefUrjNu.eEwLqwUj/xc6FjiKFsgbCJg636wjN2', 'Hello Musicpedia!', 'default.png', 'Teszt', 'Elek', '2002-04-01', 0, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -110,7 +103,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'message id', AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'message id', AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
